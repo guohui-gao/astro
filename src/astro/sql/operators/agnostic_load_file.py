4c9fdcce7833b06d269021f20bfbf6724b9cf95a
Guohui Gao <guohui_gao@hotmail.com>
2022-02-09 14:42:55 +0800

    add template

--- src/astro/sql/operators/agnostic_load_file.py
+++ src/astro/sql/operators/agnostic_load_file.py
@@ -45,7 +45,6 @@ class AgnosticLoadFile(BaseOperator):
     """
     template_fields: Iterable[str] = (
         'output_table',
-        'output_table_name',
         'output_table.table_name',
         'path',
         'task_id',
